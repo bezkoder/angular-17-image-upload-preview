83de485f0e2abd83798507fd805550e55de5baa4
tienbku <tien.ngtruong@gmail.com>
2023-12-27 12:12:46 +0700

    first commit

---
+++ src/app/components/image-upload/image-upload.component.ts
@@ -0,0 +1,75 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { FileUploadService } from '../../services/file-upload.service';
+
+@Component({
+  selector: 'app-image-upload',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './image-upload.component.html',
+  styleUrl: './image-upload.component.css',
+})
+export class ImageUploadComponent implements OnInit {
+  currentFile?: File;
+  message = '';
+  preview = '';
+
+  imageInfos?: Observable<any>;
+
+  constructor(private uploadService: FileUploadService) {}
+
+  ngOnInit(): void {
+    this.imageInfos = this.uploadService.getFiles();
+  }
+
+  selectFile(event: any): void {
+    this.message = '';
+    this.preview = '';
+    const selectedFiles = event.target.files;
+
+    if (selectedFiles) {
+      const file: File | null = selectedFiles.item(0);
+
+      if (file) {
+        this.preview = '';
+        this.currentFile = file;
+
+        const reader = new FileReader();
+
+        reader.onload = (e: any) => {
+          console.log(e.target.result);
+          this.preview = e.target.result;
+        };
+
+        reader.readAsDataURL(this.currentFile);
+      }
+    }
+  }
+
+  upload(): void {
+    if (this.currentFile) {
+      this.uploadService.upload(this.currentFile).subscribe({
+        next: (event: any) => {
+          if (event instanceof HttpResponse) {
+            this.message = event.body.message;
+            this.imageInfos = this.uploadService.getFiles();
+          }
+        },
+        error: (err: any) => {
+          console.log(err);
+
+          if (err.error && err.error.message) {
+            this.message = err.error.message;
+          } else {
+            this.message = 'Could not upload the image!';
+          }
+        },
+        complete: () => {
+          this.currentFile = undefined;
+        }
+      });
+    }
+  }
+}
